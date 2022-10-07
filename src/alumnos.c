1a6fc61e8c1d599aa3be2b5e2dbde9429075786e
alezp <polli.alejandro89@gmail.com>
2022-10-07 08:30:12 -0300

    Update alumnos.c

---
+++ src/alumnos.c
@@ -0,0 +1,64 @@
+/*=====[Module Name]===========================================================
+
+ * All rights reserved.
+ * License: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.1.0
+ * Creation Date: 2022/10/07
+ */
+ 
+/*=====[Inclusion of own header]=============================================*/
+
+#include "alumnos.h"
+#include <stdio.h>
+
+/*=====[Inclusions of private function dependencies]=========================*/
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Private function-like macros]========================================*/
+
+/*=====[Definitions of private data types]===================================*/
+
+/*=====[Definitions of external public global variables]=====================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Prototypes (declarations) of private functions]======================*/
+
+/*=====[Implementations of public functions]=================================*/
+
+/*=====[Implementations of interrupt functions]==============================*/
+
+ 
+
+bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
+    int resultado;
+    const char FORMATO[] = "{"
+        "\"documento\":\"%s\","
+        "\"apellidos\":\"%s\","
+        "\"nombres\":\"%s\""
+    "}";
+
+    resultado = snprintf(cadena, espacio, FORMATO, 
+             alumno->documento, alumno->apellidos, alumno->nombres);
+
+    return (resultado >= 0);
+
+}
+
+bool AlejandroPolli(char * cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "Polli",
+        .nombres = "Alejandro Miguel",
+        .documento = "34.764.110",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
+
+}
+/*=====[Implementations of private functions]================================*/
