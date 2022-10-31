eb5734a975938baa7e913a6746abf66825a531ce
alezp <you@example.com>
2022-10-31 11:27:44 -0300

    alumno

---
+++ src/alumno.c
@@ -0,0 +1,56 @@
+/*=====[Module Name]===========================================================
+
+ * All rights reserved.
+ * License: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.1.0
+ * Creation Date: 2022/10/07
+ */
+ 
+/*=====[Inclusion of own header]=============================================*/
+#include<stdlib.h>
+#include <"alumnos.h">
+#include <stdio.h>
+#include <string.h>
+
+
+/*=====[Inclusions of private function dependencies]=========================*/
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Private function-like macros]========================================*/
+
+/*=====[Definitions of private data types]===================================*/
+
+/*=====[Definitions of external public global variables]=====================*/
+
+/*=====[Definitions of public global variables]==============================*/
+type_def alumno alum;
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Prototypes (declarations) of private functions]======================*/
+
+/*=====[Implementations of public functions]=================================*/
+
+/*=====[Implementations of interrupt functions]==============================*/
+
+ 
+
+
+void Nombre(struct * const Alumno) 
+{
+  /*
+    const struct alumno_s alumno = {
+        .apellidos = "Polli",
+        .nombres = "Alejandro Miguel",
+        .documento = "34.764.110",
+    };
+ */
+return 0;
+}
+
+
+
+
+/*=====[Implementations of private functions]================================*/
